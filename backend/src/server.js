9d231bc34c4d2afa7c57b240c6100fad6b8f4ee7
kevalmeta <kevalmetaliya7171@gmail.com>
2025-12-11 18:09:38 +0530

    Revert "db config & tools setup"

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,10 @@
 import express from "express";
 import path from "path";
-import { clerkMiddleware } from '@clerk/express'
-
 import { ENV } from "./config/env.js";
-import { connectDB } from "./config/db.js";
 
 const app = express();
 
 const __dirname = path.resolve()
-
-app.use(clerkMiddleware()) //adds auths object under the req => req.auth
 
 app.get("/api/health",(req,res)=>{
   res.status(200).json({message:"Success"})
@@ -24,6 +19,4 @@ if(ENV.NODE_ENV==="production"){
     res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
   })
 }
-app.listen(ENV.PORT,()=>
-  console.log("Server is up and running"));
-  connectDB();
+app.listen(ENV.PORT,()=>console.log("Server is up and running"));
