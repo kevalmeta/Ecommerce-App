e3842c93c5ca858046b450c28789469ce93bc66b
kevalmeta <kevalmetaliya7171@gmail.com>
2025-12-19 11:50:42 +0530

    remove frontend serving for render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import path from "path";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 
@@ -9,26 +8,17 @@ import { functions, inngest } from "./config/inngest.js";
 
 const app = express();
 
-const __dirname = path.resolve();
+app.use(express.json());
+app.use(clerkMiddleware());
 
-app.use(express.json())
-app.use(clerkMiddleware()); //adds auths object under the req => req.auth
+// Inngest endpoint
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.use("/api/inngest",serve({client:inngest,functions}))
-
+// Health check
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
-//make our app ready for deployement
-
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../admin/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-  });
-}
 const startServer = async () => {
   await connectDB();
   app.listen(ENV.PORT, () => {
