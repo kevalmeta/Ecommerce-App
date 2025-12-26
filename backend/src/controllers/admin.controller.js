b988b1e2362dedf0dbde3cda1895b0596817ba4a
kevalmeta <kevalmetaliya7171@gmail.com>
2025-12-26 07:01:40 +0530

    tiny fixed

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -67,7 +67,7 @@ export async function updateProducts(req, res) {
 
     if (name) product.name = name;
     if (description) product.description = description;
-    if (price) product.price = parseFloat(price);
+    if (price!== undefined) product.price = parseFloat(price);
     if (stock !== undefined) product.stock = parseInt(stock);
     if (category) product.category = category;
 
