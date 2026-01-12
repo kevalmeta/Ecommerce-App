549db587d7d3c37307e5bb17caffdbd663e45a5d
kevalmeta <kevalmetaliya7171@gmail.com>
2026-01-12 19:44:01 +0530

    Login Page update

--- admin/src/pages/LoginPage.jsx
+++ admin/src/pages/LoginPage.jsx
@@ -1,11 +1,10 @@
-import { SignIn } from '@clerk/clerk-react'
+import { SignIn } from "@clerk/clerk-react";
 
 function LoginPage() {
   return (
-    <div>LoginPage
+    <div className="h-screen hero">
       <SignIn />
     </div>
-  )
+  );
 }
-
-export default LoginPage
+export default LoginPage;
