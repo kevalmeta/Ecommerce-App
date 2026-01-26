388257e840a575537ae8b78f6e946eaa657e5fda
kevalmeta <kevalmetaliya7171@gmail.com>
2026-01-26 17:15:56 +0530

    products screen is added

---
+++ mobile/hooks/useProduct.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { useApi } from "@/lib/api";
+import { Product } from "@/types";
+
+export const useProduct = (productId: string) => {
+  console.log("📦 Fetching product:", productId);
+  const api = useApi();
+  const result = useQuery<Product>({
+    queryKey: ["product", productId],
+    queryFn: async () => {
+      const { data } = await api.get(`/products/${productId}`);
+      return data;
+    },
+    enabled:!!productId
+    
+  })
+  return result;
+}
+
+
+
