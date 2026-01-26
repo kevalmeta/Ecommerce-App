6f0060c3528201f8a676080a2bdb53c1a56c4435
kevalmeta <kevalmetaliya7171@gmail.com>
2026-01-26 17:49:32 +0530

    remove commint on useProducts

--- mobile/hooks/useProduct.ts
+++ mobile/hooks/useProduct.ts
@@ -2,22 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { useApi } from "@/lib/api";
 import { Product } from "@/types";
 
-// export const useProduct = (productId: string) => {
-//   console.log("📦 Fetching product:", productId);
-//   const api = useApi();
-//   const result = useQuery<Product>({
-//     queryKey: ["product", productId],
-//     queryFn: async () => {
-//       const { data } = await api.get(`/products/${productId}`);
-//       console.log("🧾 PRODUCT RESPONSE:", data);
-//       return data;
-//     },
-//     enabled:!!productId
-    
-//   })
-//   return result;
-// }/
-
 export const useProduct = (productId: string) => {
   const api = useApi();
 
