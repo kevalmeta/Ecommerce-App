af38766f47ef5618e8c798b4d26c018f00877b3c
kevalmeta <kevalmetaliya7171@gmail.com>
2026-01-26 17:48:52 +0530

    tiny fixed in useProducts

--- mobile/hooks/useProduct.ts
+++ mobile/hooks/useProduct.ts
@@ -2,20 +2,42 @@ import { useQuery } from "@tanstack/react-query";
 import { useApi } from "@/lib/api";
 import { Product } from "@/types";
 
+// export const useProduct = (productId: string) => {
+//   console.log("📦 Fetching product:", productId);
+//   const api = useApi();
+//   const result = useQuery<Product>({
+//     queryKey: ["product", productId],
+//     queryFn: async () => {
+//       const { data } = await api.get(`/products/${productId}`);
+//       console.log("🧾 PRODUCT RESPONSE:", data);
+//       return data;
+//     },
+//     enabled:!!productId
+    
+//   })
+//   return result;
+// }/
+
 export const useProduct = (productId: string) => {
-  console.log("📦 Fetching product:", productId);
   const api = useApi();
-  const result = useQuery<Product>({
+
+  return useQuery({
     queryKey: ["product", productId],
+    enabled: !!productId,
     queryFn: async () => {
       const { data } = await api.get(`/products/${productId}`);
-      return data;
+
+      console.log("🧾 PRODUCT RESPONSE:", data);
+
+      // ✅ IMPORTANT: unwrap product
+      return {
+        ...data.product,
+        totalReviews: data.product.totalRatings ?? 0,
+      };
     },
-    enabled:!!productId
-    
-  })
-  return result;
-}
+  });
+};
+
 
 
 
